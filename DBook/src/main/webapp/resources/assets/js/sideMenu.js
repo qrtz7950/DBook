245588a275baf4c89cfa71a0e1bc3c101b327626
Ddock2 <bmkwak22@gmail.com>
2018-09-27 18:18:28 +0900

    메인 사이드메뉴 수정 / 홈.do 만들기 /

---
+++ DBook/src/main/webapp/resources/assets/js/sideMenu.js
@@ -0,0 +1,15 @@
+var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+		showLeft = document.getElementById( 'showLeft' ),
+		body = document.body;
+
+showLeft.onclick = function() {
+	classie.toggle( this, 'active' );
+	classie.toggle( menuLeft, 'cbp-spmenu-open' );
+	disableOther( 'showLeft' );
+};
+
+function disableOther( button ) {
+	if( button !== 'showLeft' ) {
+		classie.toggle( showLeft, 'disabled' );
+	}
+}
