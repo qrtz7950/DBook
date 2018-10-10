5c55f6f3fc96f31ddf3a30fcd5edf7b81950c3e5
Ddock2 <bmkwak22@gmail.com>
2018-10-10 19:50:48 +0900

    왜 안되는거냐

--- DBook/src/main/java/kr/co/project/review/service/ReviewService.java
+++ DBook/src/main/java/kr/co/project/review/service/ReviewService.java
@@ -1,5 +1,8 @@
 package kr.co.project.review.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +19,15 @@ public class ReviewService {
 		dao.writeReview(review);
 	}
 	
-	public ReviewVO[] reviewListByBook_id(String book_id) {
-		return dao.reviewListByBook_id(book_id);
+	public List<ReviewVO> reviewListByBook_id(String book_id) {
+		System.out.println(book_id);
+		List<ReviewVO> a = new ArrayList<ReviewVO>();
+		try{
+			a = dao.reviewListByBook_id(book_id);
+		}catch(Exception e){
+			System.out.println("안된다");
+		}
+		return a;
 	}
 	
 }
