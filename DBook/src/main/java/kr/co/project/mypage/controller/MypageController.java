dd0c986e22621da29686eff6a06339fb32de4201
TK_WooDong <rlawpgml7950@gmail.com>
2018-10-12 17:30:58 +0900

    회원가입후 그 정보로 바로 로그인됨 별점매기기 점수, 진행상황 바 차오르는 애니메이션 등 유저레이팅 스크립트 추가 DB 수정

--- DBook/src/main/java/kr/co/project/mypage/controller/MypageController.java
+++ DBook/src/main/java/kr/co/project/mypage/controller/MypageController.java
@@ -50,8 +50,11 @@ public class MypageController {
 	public ModelAndView userRating() {
 		System.out.println("userRating()진입");
 		
+		String[] asd = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
+		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("mypage/userRating");
+		mav.addObject("asd",asd);
 		
 		return mav;
 	}
