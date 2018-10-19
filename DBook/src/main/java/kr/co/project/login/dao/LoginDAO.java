71555ba23ced62e396fbd2f18147f37fc4346d3d
TK_WooDong <rlawpgml7950@gmail.com>
2018-10-19 17:48:23 +0900

    -내 서재 css 파일 분리 -Login 패키지 인터페이스 제거 -회원가입시 닉네임 입력되도록 코드 수정

--- DBook/src/main/java/kr/co/project/login/dao/LoginDAO.java
+++ DBook/src/main/java/kr/co/project/login/dao/LoginDAO.java
@@ -1,52 +1,56 @@
 package kr.co.project.login.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import kr.co.project.login.vo.LoginVO;
 
-public interface LoginDAO {
+@Repository
+public class LoginDAOImpl implements LoginDAO{
 
-	/**
-	 * 카카오 api를 이용해 로그인한 사용자의 회원가입을 위한 메소드
-	 * @param user
-	 */
-	public void signIn(LoginVO user);
-	
-	/**
-	 * 이미 가입되어 있는 유저인지 체크
-	 * @param user
-	 * @return
-	 */
-	public boolean overlapCheck(LoginVO user);
-	
-	/**
-	 * 파라미터의 정보대로 회원의 정보를 수정
-	 * @param user
-	 */
-	public void updateUserInfo(LoginVO user);
-	
-	/**
-	 * 일반 사용자용 로그인
-	 * @param user
-	 * @return
-	 */
-	public LoginVO login(LoginVO user);
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
 
-	/**
-	 * 카카오 채널링 유저 로그인
-	 * @param user
-	 * @return
-	 */
-	public LoginVO kakaoLogin(LoginVO user);
-
-	/**
-	 * 추가로 정보를 입력한 채널링 유저의 정보추가를 위한 메소드
-	 * @param user
-	 */
-	public void addUserInfo(LoginVO user);
-
-	/**
-	 * 카카오 채널링 유저 회원가입
-	 * @param user
-	 */
-	public void kakaoSignIn(LoginVO user);
+	@Override
+	public void signIn(LoginVO user) {
+		sqlSessionTemplate.insert("kr.co.project.login.dao.LoginDAO.signIn", user);
+	}
 	
+	@Override
+	public void kakaoSignIn(LoginVO user) {
+		sqlSessionTemplate.insert("kr.co.project.login.dao.LoginDAO.kakaoSignIn", user);
+	}
+
+	@Override
+	public LoginVO login(LoginVO user) {
+		user = sqlSessionTemplate.selectOne("kr.co.project.login.dao.LoginDAO.login", user);
+		return user;
+	}
+
+	@Override
+	public boolean overlapCheck(LoginVO user) {
+		LoginVO isOverlap = sqlSessionTemplate.selectOne("kr.co.project.login.dao.LoginDAO.overlapCheck", user);
+		if(isOverlap == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	@Override
+	public void updateUserInfo(LoginVO user) {
+		sqlSessionTemplate.selectOne("kr.co.project.login.dao.LoginDAO.update", user);
+	}
+
+	@Override
+	public LoginVO kakaoLogin(LoginVO user) {
+		user = sqlSessionTemplate.selectOne("kr.co.project.login.dao.LoginDAO.kakaoLogin", user);
+		return user;
+	}
+
+	@Override
+	public void addUserInfo(LoginVO user) {
+		user = sqlSessionTemplate.selectOne("kr.co.project.login.dao.LoginDAO.updateAll", user);
+	}
 }
--- DBook/src/main/java/kr/co/project/login/service/LoginService.java
+++ DBook/src/main/java/kr/co/project/login/service/LoginService.java
@@ -1,43 +1,69 @@
 package kr.co.project.login.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.project.login.dao.LoginDAOImpl;
 import kr.co.project.login.vo.LoginVO;
 
-public interface LoginService {
-	
-	/**
-	 * 카카오 api를 이용한 로그인을 위한 method
-	 * 가입되어 있지 않은 유저일 경우 DB에 정보를 추가한다
-	 */
-	
-	public LoginVO Login(LoginVO user);
+@Service
+public class LoginServiceImpl implements LoginService{
 
-	/**
-	 * 메인에서 추가로 입력된 추가정보를 매개변수 객체의 id에 추가
-	 * @param user
-	 * @return
-	 */
+	@Autowired
+	private LoginDAOImpl dao;
 	
-	public LoginVO addUserInfo(LoginVO user);
-
-	/**
-	 * 일반 유저 회원가입 썸네일, 프로필이미지, 닉네임 등의 정보는 제외된다
-	 * @param user
-	 * @return
-	 */
-	public LoginVO signIn(LoginVO user);
+	@Override
+	public LoginVO Login(LoginVO user) {
+		user = dao.login(user);
+		return user;
+	}
 	
-	/**
-	 * 카카오 채널링 유저 로그인
-	 * @param user
-	 * @return
-	 */
-	public LoginVO kakaoLogin(LoginVO user);
+	@Override
+	public LoginVO kakaoLogin(LoginVO user) {
+		
+		if(dao.overlapCheck(user)) {
+			dao.updateUserInfo(user);
+			user = dao.kakaoLogin(user);
+		} else {
+			dao.kakaoSignIn(user);
+		}
+		
+		return user;
+	}
+	@Override
+	public LoginVO addUserInfo(LoginVO user) {
+		
+		dao.addUserInfo(user);
+		user = dao.login(user);
+		return user;
+	}
+	
+	@Override
+	public LoginVO signIn(LoginVO user) {
+		
+		if(dao.overlapCheck(user)) {
+			user = null;
+		} else {
+			dao.signIn(user);
+			user = dao.login(user);
+		}
+		return user;
+	}
 
-	/**
-	 * ajax로 구현된 id 중복체크를 위해 스트링을 반환하는 리스폰스 메소드 
-	 * @param id
-	 * @return
-	 */
-	 public String idDupCheck(LoginVO user);
-
+	@Override
+	public String idDupCheck(LoginVO user) {
+		
+		String idDupCheck = null;
+		
+		if(dao.overlapCheck(user)) {
+			idDupCheck = "중복된 ID입니다";
+		} else {
+			idDupCheck = "사용하셔도 좋은 ID입니다";
+		}
+		
+		return idDupCheck;
+	}
+	
+	
+	
 }
